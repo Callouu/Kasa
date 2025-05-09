915f24c22614c00de1e97408f62ba50583e453d1
Alexis Bouvry <bouvryalexis@gmail.com>
2025-05-09 21:15:49 +0200

    add routes

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import Layout from "../layouts/Layout"
+import Home from "../pages/home/home";
+import Fiche from '../pages/Fiche-logement/Fiche';
+import APropos from "../pages/A-propos/APropos";
+import Erreur404 from "../pages/404/404";
 
-function Routes() {
-  return (
-    <div>Routes</div>
-  )
+
+function AllRoutes() {
+    return(
+        <HashRouter>
+            <Layout>
+                <Routes>
+                    <Route element={<Navigate replace to="/accueil" />} path="/" />
+                    <Route path="/accueil" element={<Home />}/>
+                    <Route path="/logement/:id" element={<Fiche />}/>
+                    <Route path="/a-propos" element={<APropos />}/>
+                    <Route path="*" element={<Erreur404 />}/>
+                </Routes>
+            </Layout>
+        </HashRouter>
+    );
 }
 
-export default Routes
+export default AllRoutes;
