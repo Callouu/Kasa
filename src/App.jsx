48bdce442d5fed398432bfcde61abd5c3db4e921
Alexis Bouvry <bouvryalexis@gmail.com>
2025-05-11 11:45:44 +0200

    add new routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router'
+import { Navigate } from 'react-router'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Error from './components/Error'
 import Home from './pages/Home'
+import AboutKasa from './pages/About'
 
 
 function App() {
-
   return (
     <BrowserRouter>
       <Header />
       <main>
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route path="/a-propos" element={<AboutKasa />} />
+          <Route path="/adresse-introuvable" element={<Error />} />
+          <Route path="*" element={<Navigate to="/adresse-introuvable" replace />}
+          />
         </Routes>
       </main>
       <Footer />
