4c42bc891653218d7b7e20f3ac1fa1ade3426d6c
Alexis Bouvry <bouvryalexis@gmail.com>
2025-05-11 11:34:44 +0200

    add card homepage

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,10 @@
+function Card({ title, picture }) {
+  return (
+    <div className="card">
+      <img className="card__img" src={picture} alt="" />
+      <h2 className="card__title"> {title} </h2>
+    </div>
+  )
+}
+
+export default Card
