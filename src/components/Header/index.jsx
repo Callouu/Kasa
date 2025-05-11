70bc9924a0a51482b84f1c05677b6f57c94e241e
Alexis Bouvry <bouvryalexis@gmail.com>
2025-05-11 11:30:34 +0200

    add header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import { Link, NavLink } from 'react-router'
+import logo from '../../assets/logo.svg'
+
+const navLink = [
+  { name: 'Accueil', href: '/' },
+  { name: 'A Propos', href: '/a-propos' },
+]
 
 function Header() {
   return (
-    <div> Header</div>
+    <header className="header">
+      <Link to="/">
+        <img className="header__logo" src={logo} alt="Kasa" />
+      </Link>
+      <nav className="header__nav">
+        {navLink.map((item) => (
+          <NavLink
+            to={item.href}
+            key={item.name}
+            className={({ isActive }) => {
+              return isActive ? 'active-link' : ''
+            }}
+          >
+            {item.name}
+          </NavLink>
+        ))}
+      </nav>
+    </header>
   )
 }
 
