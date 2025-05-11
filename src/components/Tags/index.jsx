c8c84009b7e969665004c542c9cc7f5ff539c96d
Alexis Bouvry <bouvryalexis@gmail.com>
2025-05-11 11:54:24 +0200

    add components to rental page

---
+++ src/components/Tags/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+//Component
+function Tags({ tag }) {
+  return (
+    <div>
+      <ul className="tags">
+        {tag.map((tag, index) => (
+          <li className="tags__text" key={index}>
+            {tag}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default Tags
