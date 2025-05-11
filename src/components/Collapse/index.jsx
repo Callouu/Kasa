659bc74c50ed90c7e6561c9f3fca6d4d08814e5a
Alexis Bouvry <bouvryalexis@gmail.com>
2025-05-11 11:45:19 +0200

    collapse component

---
+++ src/components/Collapse/index.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import expandArrow from '../../assets/expand_arrow.svg'
+
+
+function Collapse({ title, text, className }) {
+  const [open, setOpen] = useState(false)
+  const toggle = () => {
+    setOpen(!open)
+  }
+  return (
+    <div className={className}>
+      <button className="collapse__title-container" onClick={toggle}>
+        <h2 className="collapse__title-container__title">{title}</h2>
+        <img
+          className={!open ? 'expand_icon' : 'expand_icon expand_icon--opened'}
+          src={expandArrow}
+          alt="Cliquez-ici pour dérouler le texte"
+        />
+      </button>
+      {open && <div className="collapse__text">{text}</div>}
+    </div>
+  )
+}
+
+export default Collapse
