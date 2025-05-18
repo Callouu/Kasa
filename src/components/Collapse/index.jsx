e09fbd1cc49f7bfd6289a9fa971e503967db8f90
Alexis Bouvry <bouvryalexis@gmail.com>
2025-05-18 11:32:30 +0200

    add style to collapse text

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,25 +1,24 @@
-import React, { useState } from 'react'
-import expandArrow from '../../assets/expand_arrow.svg'
-
+import React, { useState } from "react";
+import expandArrow from "../../assets/expand_arrow.svg";
 
 function Collapse({ title, text, className }) {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
   const toggle = () => {
-    setOpen(!open)
-  }
+    setOpen(!open);
+  };
   return (
     <div className={className}>
       <button className="collapse__title-container" onClick={toggle}>
         <h2 className="collapse__title-container__title">{title}</h2>
         <img
-          className={!open ? 'expand_icon' : 'expand_icon expand_icon--opened'}
+          className={!open ? "expand_icon" : "expand_icon expand_icon--open"}
           src={expandArrow}
           alt="Cliquez-ici pour dérouler le texte"
         />
       </button>
       {open && <div className="collapse__text">{text}</div>}
     </div>
-  )
+  );
 }
 
-export default Collapse
+export default Collapse;
